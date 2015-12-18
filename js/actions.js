184485a05032f7e6b0fe3880808ba3bd7ac07123
particular <pemijumo@gmail.com>
2015-12-18 14:49:04 -0600

    56

--- js/actions.js
+++ js/actions.js
@@ -1,4 +1,30 @@
-//actions
 var jQT = new $.jQTouch({
     themeSelectionSelector: '#jqt'
 });
+
+var fn = {
+
+    init: function () {
+        $('#ncSend').tap(fn.crearContacto);
+        $('#cListar').tap(cn.findContacts);
+        $('#aEscribir').tap(f.createFile);
+        $('#aLeer').tap(f.readFile);
+    },
+
+    crearContacto: function () {
+        var n = $('#ncNom').val();
+        var t = $('#ncTel').val();
+        var m = $('#ncMail').val();
+
+        if (n != '' && t != '' && m != '')
+            cn.createContact(n, t, m);
+        else
+            alert("campos requeridos");
+
+    },
+    ready: function () {
+        document.addEventListener("deviceready", fn.init, false);
+    }
+};
+
+$(fn.ready);
