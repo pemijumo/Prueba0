2dbe278f0bd56e753b0077f18a126ddd5d6aa36c
particular <pemijumo@gmail.com>
2015-12-18 10:52:08 -0600

    app new prueba
    prueba 1

---
+++ CS.aspx.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Xml;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    //private static string connectionStringPJ = @"Data Source=192.168.2.205;Initial Catalog=SBO-DistPJ; user id= sa; password = SAP-PJ1; TrustServerCertificate=true;";
+    //private static string connectionStringPJ = @"Data Source=192.168.2.204;Initial Catalog=SBO-DistPJ; user id= sa; password = SAP-PJ1; TrustServerCertificate=true;";
+    private static string connectionStringPJ = @"Data Source=192.168.2.100;Initial Catalog=SBO-DistPJ; user id= sa; password = SAP-PJ1; TrustServerCertificate=true;";
+
+    public static decimal PrecioCompra;
+
+    public enum TipoConsulta
+    {
+        Pesos = 1,
+        Usd = 2,
+        ListaPercios = 3,
+        Articulos = 4,
+        Stocks = 5,
+        TC = 6
+    }
+
+    public enum Columnas
+    {
+        PriceList,
+        MXP, USD
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    [System.Web.Services.WebMethod]
+    public static string GetPrice(string ItemCode, decimal Utilidad)
+    {
+        decimal PrecioCompra = decimal.Zero;
+        decimal Porcentaje = decimal.Zero;
+        try
+        {
+            SqlConnection connection = new SqlConnection(connectionStringPJ);
+
+            Porcentaje = Utilidad;
+
+            SqlCommand command = new SqlCommand("PJ_CalculoUtilidad", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@TipoConsulta", 1);
+            command.Parameters.AddWithValue("@Articulo", ItemCode);
+
+            SqlParameter PrecCompra = new SqlParameter("@PrecioCompra", decimal.Zero);
+            PrecCompra.Direction = ParameterDirection.Output;
+            PrecCompra.DbType = DbType.Decimal;
+            PrecCompra.Scale = 6;
+            command.Parameters.Add(PrecCompra);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+
+            PrecioCompra = Convert.ToDecimal(command.Parameters["@PrecioCompra"].Value.ToString());
+            connection.Close();
+
+            return decimal.Round((PrecioCompra / (100 - Porcentaje)) * 100, 2).ToString("C2");
+        }
+        catch (Exception ex)
+        {
+            return decimal.Zero.ToString ("C2");
+        }
+    }
+
+    [System.Web.Services.WebMethod]
+    //[System.Web.Script.Services.ScriptMethod(ResponseFormat = System.Web.Script.Services.ResponseFormat.Json)]
+    public static string[] AutoComplete(string CodArticulo, int Opcion)
+    {
+        List<string> customers = new List<string>();
+        try
+        {
+            SqlConnection connection = new SqlConnection(connectionStringPJ);
+
+            SqlCommand command = new SqlCommand("PJ_CalculoUtilidad", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@TipoConsulta", 7);
+            command.Parameters.AddWithValue("@Articulo", CodArticulo);
+            command.Parameters.AddWithValue("@PrecioCompra", 0);
+            command.Parameters.AddWithValue("@OpcionAutocompletado", Opcion);
+
+            connection.Open();
+            using (SqlDataReader sdr = command.ExecuteReader())
+            {
+                while (sdr.Read())
+                {
+                    if (Opcion == 1)
+                        customers.Add(string.Format("{0}", sdr["ItemCode"]));
+                    if (Opcion == 2)
+                        customers.Add(string.Format("{0}", sdr["Dscription"]));
+                }              
+                
+            }
+
+            connection.Close();
+            
+        }
+        catch (Exception ex)
+        {
+            //customers.Add(string.Format("{0}", ex.Message.ToString()));
+            //return customers.ToArray();
+        }
+        return customers.ToArray();       
+    }
+
+    [System.Web.Services.WebMethod]
+    //[System.Web.Script.Services.ScriptMethod(ResponseFormat = System.Web.Script.Services.ResponseFormat.Json)]
+    public static List<Articulo> AutoCompleteAll(string CodArticulo)
+    {
+        List<Articulo> customers = new List<Articulo>();
+        try
+        {
+            SqlConnection connection = new SqlConnection(connectionStringPJ);
+
+            SqlCommand command = new SqlCommand("PJ_CalculoUtilidad", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@TipoConsulta", 9);
+            command.Parameters.AddWithValue("@Articulo", "");
+            command.Parameters.AddWithValue("@PrecioCompra", 0);
+            connection.Open();
+            using (SqlDataReader sdr = command.ExecuteReader())
+            {
+                while (sdr.Read())
+                {
+                    Articulo obj = new Articulo();
+                    obj.ItemCode = sdr["ItemCode"].ToString();
+                    obj.Dscription = sdr["Dscription"].ToString();
+                    customers.Add(obj);                  
+                }
+            }
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+          
+        }
+        return customers;
+    }
+
+    [System.Web.Services.WebMethod]
+    public static string GetCurrentTime(string name)
+    {
+        return "Hello " + name + Environment.NewLine + "The Current Time is: "
+            + DateTime.Now.ToString();
+    }
+
+    [System.Web.Services.WebMethod]
+    public static List<Precios> ConsultaPrecios(string DescripArticulo, int TipoConsulta, int BDescripcion)
+    {
+        Precios objPrecio = new Precios();
+        List<Precios> lstPrecios = new List<Precios>();
+
+        string CodArticulo = "--------------------";
+        if (BDescripcion == 1)
+            CodArticulo = ObtenerCodigo(8, DescripArticulo);
+        else
+            CodArticulo = DescripArticulo;
+
+        SqlConnection connection = new SqlConnection(connectionStringPJ);
+
+        SqlCommand command = new SqlCommand("PJ_CalculoUtilidad", connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@TipoConsulta", TipoConsulta);
+        command.Parameters.AddWithValue("@Articulo", CodArticulo);
+        command.Parameters.AddWithValue("@PrecioCompra", 0.0);
+
+        connection.Open();
+        using (SqlDataReader sdr = command.ExecuteReader())
+        {
+            while (sdr.Read())
+            {
+                objPrecio = new Precios();
+                objPrecio.ListName = sdr["Lista de precios"].ToString();
+                objPrecio.MXP = Convert.ToDecimal(sdr["MXP"]).ToString("C2");
+                objPrecio.USD = Convert.ToDecimal(sdr["USD"]).ToString("C2");
+                lstPrecios.Add(objPrecio);
+            }
+        }
+        connection.Close();
+        return lstPrecios;
+     
+    }
+
+    [System.Web.Services.WebMethod]
+    public static List<Stocks> ConsultaStocks(string DescripArticulo, int TipoConsulta, int BDescripcion)
+    {
+        Stocks objStock = new Stocks();
+        List<Stocks> lstStock = new List<Stocks>();
+
+        string CodArticulo = "--------------------";
+        if (BDescripcion == 1)
+            CodArticulo = ObtenerCodigo(8, DescripArticulo);
+        else
+            CodArticulo = DescripArticulo;
+
+        SqlConnection connection = new SqlConnection(connectionStringPJ);
+
+        SqlCommand command = new SqlCommand("PJ_CalculoUtilidad", connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@TipoConsulta", TipoConsulta);
+        command.Parameters.AddWithValue("@Articulo", CodArticulo);
+        command.Parameters.AddWithValue("@PrecioCompra", 0.0);
+
+        connection.Open();
+        using (SqlDataReader sdr = command.ExecuteReader())
+        {
+            while (sdr.Read())
+            {
+                objStock = new Stocks();
+                objStock.Almacen = sdr["Almacen"].ToString();
+                objStock.Stock = Convert.ToDecimal(sdr["Stock"]).ToString("N0");
+                objStock.Solicitado = Convert.ToDecimal(sdr["Solicitado"]).ToString("N0");
+                lstStock.Add(objStock);
+            }
+        }
+        connection.Close();
+        return lstStock;
+
+    }
+
+    [System.Web.Services.WebMethod]
+    public static string CalculaUtilidadPrecio(int TipoConsulta, string CodArticulo, int TipoMoneda, string Monto)
+    {
+
+        string Util = "";
+        decimal MontoMoneda = Convert.ToDecimal(Monto);
+        //decimal Usd = Convert.ToDecimal(Monto);
+       // decimal Porcentaje = Convert.ToDecimal(Monto);
+
+        SqlConnection connection = new SqlConnection(connectionStringPJ);
+
+        SqlCommand command = new SqlCommand("PJ_CalculoUtilidad", connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@TipoConsulta", TipoConsulta);
+        command.Parameters.AddWithValue("@Articulo", CodArticulo);
+        SqlParameter PrecCompra = new SqlParameter("@PrecioCompra", decimal.Zero);
+        PrecCompra.Direction = ParameterDirection.Output;
+        PrecCompra.DbType = DbType.Decimal;
+        PrecCompra.Scale = 6;
+        command.Parameters.Add(PrecCompra);
+
+        connection.Open();
+        command.ExecuteNonQuery();
+        PrecioCompra = Convert.ToDecimal(command.Parameters["@PrecioCompra"].Value.ToString());
+        connection.Close();
+
+        Util = decimal.Round((((PrecioCompra / MontoMoneda) - 1) * -100), 2).ToString() /*+ "%"*/;       
+
+        return Util;
+        
+
+    }
+
+
+    
+    [System.Web.Services.WebMethod]
+    public static string CalculaUtilidadPorciento(int TipoConsulta, string DescripArticulo, int TipoMoneda, string Monto, int BDescripcion)
+    {
+
+        string Util = "";
+        //decimal Pesos = Convert.ToDecimal(Monto);
+        //decimal Usd = Convert.ToDecimal(Monto);
+        decimal Porcentaje = Convert.ToDecimal(Monto);
+
+        string CodArticulo = "--------------------";
+        if (BDescripcion == 1)
+            CodArticulo = ObtenerCodigo(8, DescripArticulo);
+        else
+            CodArticulo = DescripArticulo;
+
+        SqlConnection connection = new SqlConnection(connectionStringPJ);
+
+        SqlCommand command = new SqlCommand("PJ_CalculoUtilidad", connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@TipoConsulta", TipoConsulta);
+        command.Parameters.AddWithValue("@Articulo", CodArticulo);
+        SqlParameter PrecCompra = new SqlParameter("@PrecioCompra", decimal.Zero);
+        PrecCompra.Direction = ParameterDirection.Output;
+        PrecCompra.DbType = DbType.Decimal;
+        PrecCompra.Scale = 6;
+        command.Parameters.Add(PrecCompra);
+
+        connection.Open();
+        command.ExecuteNonQuery();
+        PrecioCompra = Convert.ToDecimal(command.Parameters["@PrecioCompra"].Value.ToString());
+        connection.Close();
+        if (100 - Porcentaje == 0)
+            Util = PrecioCompra.ToString();
+        else
+            Util = decimal.Round((PrecioCompra / (100 - Porcentaje)) * 100, 2).ToString(/*"C2"*/);
+        return Util;
+
+
+    }
+    
+    [System.Web.Services.WebMethod]
+    public static string ObtenerDescripcionArticulo(int TipoConsulta, string CodArticulo)
+    {
+        string Descripcion = "";
+
+        SqlConnection connection = new SqlConnection(connectionStringPJ);
+
+        SqlCommand command = new SqlCommand("PJ_CalculoUtilidad", connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@TipoConsulta", TipoConsulta);
+        command.Parameters.AddWithValue("@Articulo", CodArticulo);
+        command.Parameters.AddWithValue("@PrecioCompra", 0.0);
+
+        connection.Open();
+        using (SqlDataReader sdr = command.ExecuteReader())
+        {
+            while (sdr.Read())
+            {
+                Descripcion = sdr["Dscription"].ToString();
+            }
+        }
+
+        return Descripcion;
+    }
+
+
+    public static string ObtenerCodigo(int TipoConsulta, string CodArticulo)
+    {
+        string Codigo = "---------------------";
+
+        SqlConnection connection = new SqlConnection(connectionStringPJ);
+
+        SqlCommand command = new SqlCommand("PJ_CalculoUtilidad", connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@TipoConsulta", TipoConsulta);
+        command.Parameters.AddWithValue("@Articulo", CodArticulo);
+        command.Parameters.AddWithValue("@PrecioCompra", 0.0);
+
+        connection.Open();
+        using (SqlDataReader sdr = command.ExecuteReader())
+        {
+            while (sdr.Read())
+            {
+                Codigo = sdr["ItemCode"].ToString();
+            }
+        }
+
+        return Codigo;
+    }
+
+    public class Articulo
+    {
+        public string ItemCode { get; set; }
+        public string Dscription { get; set; }
+ 
+    }
+
+    public class Precios
+    {
+        public string ListName { get; set; }
+        public string MXP { get; set; }
+        public string USD { get; set; } 
+    }
+
+    public class Stocks
+    {
+        public string Almacen { get; set; }
+        public string Stock { get; set; }
+        public string Solicitado { get; set; }
+    }
+}
---
+++ js/Jactions.js
@@ -0,0 +1,374 @@
+﻿var CodArt = []; var NomArt = [];
+//var urlDOM = ""; var Publi = 0;
+//var urlDOM = "http://192.168.2.204:90/"; var Publi = 1;
+var urlDOM = "http://187.237.98.114:80/"; var Publi = 1;
+
+
+$(document).bind("pageinit", function () {
+
+    /* //Codigo para efecto nieve
+    $.fn.snow({
+    minSize: 10, //Tamaño mínimo del copo de nieve, 10 por defecto
+    maxSize: 20, //Tamaño máximo del copo de nieve, 10 por defecto
+    newOn: 1250, //Frecuencia (en milisegundos) con la que aparecen los copos de nieve, 500 por defecto
+    flakeColor: '#FFFFFF' //Color del copo de nieve, #FFFFFF por defecto
+    });
+    */
+
+    //$("#Radio1").attr('checked', false);
+
+
+    $("#btnBusqArt").bind("click", function (event, ui) {
+        var Codigo = $("#txtItemCode").val();
+        if (Publi == 1) {
+            $.mobile.loading('show', {
+                text: 'Consultando...',
+                textVisible: true,
+                theme: 'a',
+                html: ""
+            });
+        }
+        VerificaDescripcionArticulo(Codigo);
+
+
+    });
+
+
+    $("#btnLimpArt").bind("click", function (event, ui) {
+        $("#txtItemCode").val('');
+        $("#txtPrecio").val('');
+        $("#txtUtilidad").val('');
+        $("#sDescripcionArticulo").text('');
+
+        //--------------------------------------
+        var htmlTable = '';
+        $("#tblLista").html('');
+        $("#tblLista").html(htmlTable);
+
+        $("#tblListaStock").html('');
+        $("#tblListaStock").html(htmlTable);
+        $('#idInformacion').css('display', 'none');
+
+        if (Publi == 1) {
+            $.mobile.loading('hide');
+        }
+    });
+
+
+    $("#btnAplicPrecio").bind("click", function (event, ui) {
+        var CodORNom = "";
+        CodORNom = $("#txtItemCode").val();
+        if (CodORNom == "")
+            CodORNom = $("#txtItemName").val();
+
+        if (CodORNom != "") {
+            var mon = $("#txtPrecio").val();
+            if (mon != "") {
+                var code = CodORNom;
+                var TipoMoneda = -1;
+                var TipoConsulta = -1;
+                if ($('#rbtPesos').is(':checked')) {
+                    TipoMoneda = 1;
+                    TipoConsulta = 1;
+                }
+                if ($('#rbtDolares').is(':checked')) {
+                    TipoMoneda = 2;
+                    TipoConsulta = 2;
+                }
+
+                if (mon != "") {
+                    if (Publi == 1) {
+                        $.mobile.loading('show', {
+                            text: 'Calculando...',
+                            textVisible: true,
+                            theme: 'a',
+                            html: ""
+                        });
+                    }
+                    //Se llena la tabla de precios
+                    $.ajax({
+                        url: urlDOM + "CS.aspx/CalculaUtilidadPrecio",
+                        data: "{ TipoConsulta: " + TipoConsulta + ", CodArticulo: '" + code + "'" + ", TipoMoneda: " + TipoMoneda + ", Monto: '" + mon + "'}",
+                        dataType: "json",
+                        type: "POST",
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function (data) {
+                            $("#txtUtilidad").val(data.d);
+                            if (Publi == 1) {
+                                $.mobile.loading('hide');
+                            }
+                        }
+                    });
+                }
+            }
+            else {
+                Mensaje("Debe especificar un monto", "HalcoNET", "Aceptar");
+            }
+
+        }
+        else {
+            Mensaje("Debe consultar un articulo por código", "HalcoNET", "Aceptar");
+        }
+    });
+
+    //----------------------------------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+
+    $("#btnAplicUtilidad").bind("click", function (event, ui) {
+        var CodORNom = "";
+        CodORNom = $("#txtItemCode").val();
+        if (CodORNom == "")
+            CodORNom = $("#txtItemName").val();
+        if (CodORNom != "") {
+            var mon = $("#txtUtilidad").val();
+            if (mon != "") {
+                var code = $("#txtItemCode").val();
+                var TipoMoneda = -1;
+                var TipoConsulta = -1;
+                if ($('#rbtPesos').is(':checked')) {
+                    TipoMoneda = 1;
+                    TipoConsulta = 1;
+                }
+                if ($('#rbtDolares').is(':checked')) {
+                    TipoMoneda = 2;
+                    TipoConsulta = 2;
+                }
+                if (mon != "") {
+                    if (Publi == 1) {
+                        $.mobile.loading('show', {
+                            text: 'Calculando...',
+                            textVisible: true,
+                            theme: 'a',
+                            html: ""
+                        });
+                    }
+                    //Se llena la tabla de precios
+                    $.ajax({
+                        url: urlDOM + "CS.aspx/CalculaUtilidadPorciento",
+                        data: "{ TipoConsulta: " + TipoConsulta + ", DescripArticulo: '" + code + "'" + ", TipoMoneda: " + TipoMoneda + ", Monto: '" + mon + "'" + ", BDescripcion:" + 0 + "}",
+                        dataType: "json",
+                        type: "POST",
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function (data) {
+                            $("#txtPrecio").val(data.d);
+                            if (Publi == 1) {
+                                $.mobile.loading('hide');
+                            }
+                        }
+                    });
+                }
+            }
+            else {
+                Mensaje("Debe especificar un monto para la utilidad", "HalcoNET", "Aceptar");
+            }
+        }
+        else {
+            Mensaje("Debe consultar un articulo por código", "HalcoNET", "Aceptar");
+        }
+    });
+});                       //cierre de página
+
+
+
+function Llena() {
+    var str = "";
+    var sugList = $('[data-role=listview]');
+
+    $.ajax({
+        url: urlDOM + "CS.aspx/AutoCompleteAll",
+        data: "{CodArticulo: '" + 1 + "'}",
+        dataType: "json",
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            if (data != null && $.isArray(data.d)) {
+                $.each(data.d, function (index, value) {
+                    CodArt.push(value.ItemCode);
+                    NomArt.push(value.Dscription);
+                    str += "<li class=ui-screen-hidden><a href='#'>" + value.ItemCode + "</a></li>";
+                    sugList.html(str);
+                    sugList.listview("refresh");
+                });
+            }
+        }
+    });
+}
+
+function Llena2() {
+    var str = "";
+    var sugList = $("#sugges1");
+    $.ajax({
+        url: urlDOM + "CS.aspx/AutoCompleteAll",
+        data: "{CodArticulo: '" + 1 + "'}",
+        dataType: "json",
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            var str = "";
+            if (data != null && $.isArray(data.d)) {
+                $.each(data.d, function (index, value) {
+                    str += "<li><a href='#'>" + value.ItemCode + "</a></li>";
+                    sugList.html(str);
+                    sugList.listview("refresh");
+                    
+//                    CodArt.push(value.ItemCode);
+//                    NomArt.push(value.Dscription);
+                });
+            
+            }
+        }
+    });
+}
+
+
+
+function Llena3() {
+    var str = "";
+    var sugList = $("#sugges1");
+    if (CodArt != null && $.isArray(CodArt)) {
+        $.each(CodArt, function (index, value) {
+            str += "<li class=ui-screen-hidden><a href='#'>" + value + "</a></li>";
+            sugList.html(str);
+            sugList.listview("refresh");
+        });
+    }        
+}
+
+
+
+function ConsultaListaPrecios(Articulo, TipoConsulta, BDescripcion) {
+    //Se llena la tabla de precios
+    $.ajax({
+        url: urlDOM + "CS.aspx/ConsultaPrecios", /* Llamamos a tu archivo */
+        data: "{ 'DescripArticulo': '" + Articulo + "', TipoConsulta:" + TipoConsulta + ", BDescripcion:" + BDescripcion + "}", /* Ponemos los parametros de ser necesarios */
+        dataType: "json",
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",  /* Esto es lo que indica que la respuesta será un objeto JSon */
+        success: function (data) {
+            /* Supongamos que #contenido es el tbody de tu tabla */
+            /* Inicializamos tu tabla */
+            var htmlTable = '';
+            $("#tblLista").html('');
+            /* Vemos que la respuesta no este vacía y sea una arreglo */
+            if (data != null && $.isArray(data.d)) {
+                /* Recorremos tu respuesta con each */
+                htmlTable += '<table class="phone-compare ui-shadow table-stroke">';
+                $.each(data.d, function (index, value) {
+                    /* Vamos agregando a nuestra tabla las filas necesarias */
+                    htmlTable += '<tr>';
+                    htmlTable += '<td class="col-stock-Izq">' + value.ListName + '</td>';
+                    htmlTable += '<td class="col-stock-Der">' + value.MXP + '</td>';
+                    htmlTable += '<td class="col-stock-Der">' + value.USD + '</td>';
+                    htmlTable += '</tr>';
+                    //$("#tblLista").append("<tr><td>" + value.ListName + "</td></tr>");
+                    //i++;
+                });
+
+                htmlTable += '</table>';
+                $("#tblLista").html(htmlTable);
+            }
+        }
+    });
+}
+
+function ConsultaStock(Articulo, TipoConsulta, BDescripcion) {
+    //Se llena la tabla de precios
+    $.ajax({
+        url: urlDOM + "CS.aspx/ConsultaStocks", /* Llamamos a tu archivo */
+        data: "{ 'DescripArticulo': '" + Articulo + "', TipoConsulta:" + TipoConsulta + ", BDescripcion:" + BDescripcion + "}", /* Ponemos los parametros de ser necesarios */
+        dataType: "json",
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",  /* Esto es lo que indica que la respuesta será un objeto JSon */
+        success: function (data) {
+            /* Supongamos que #contenido es el tbody de tu tabla */
+            /* Inicializamos tu tabla */
+            var htmlTable = '';
+            $("#tblListaStock").html('');
+            /* Vemos que la respuesta no este vacía y sea una arreglo */
+            if (data != null && $.isArray(data.d)) {
+                /* Recorremos tu respuesta con each */
+                htmlTable += '<table>';
+                $.each(data.d, function (index, value) {
+                    /* Vamos agregando a nuestra tabla las filas necesarias */
+                    htmlTable += '<tr>';
+                    htmlTable += '<td class="col-stock">' + value.Almacen + '</td>';
+                    htmlTable += '<td class="col-stock">' + value.Stock + '</td>';
+                    htmlTable += '<td class="col-stock">' + value.Solicitado + '</td>';
+                    htmlTable += '</tr>';
+                    //$("#tblLista").append("<tr><td>" + value.ListName + "</td></tr>");
+                    //i++;
+                });
+
+                htmlTable += '</table>';
+                $("#tblListaStock").html(htmlTable);
+            }
+        }
+    });
+}
+
+
+function VerificaDescripcionArticulo(Codigo) {
+    var result = "";
+    if (Codigo != "") {
+        $.ajax({
+            url: urlDOM + "CS.aspx/ObtenerDescripcionArticulo",
+            data: "{ TipoConsulta: " + 9 + ", CodArticulo:'" + Codigo + "'}",
+            dataType: "json",
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            success: function (response) {
+                result = response.d;
+                if (result != "") {                    
+                    $('#idInformacion').css('display', 'block');
+                    $("#sDescripcionArticulo").text(result);
+                    ConsultaListaPrecios(Codigo, 3, 0);
+                    ConsultaStock(Codigo, 5, 0);
+                    if (Publi == 1) {
+                        $.mobile.loading("hide");
+                    }
+                }
+                else {
+                    Mensaje("No existe ningun articulo con el código especificado", "HalcoNET", "Aceptar");
+                    LimpiaCodArtNoExistente();
+                    $('#idInformacion').css('display', 'none');
+                    
+                }
+            }
+        });
+    }
+    else {
+        Mensaje("Debe especificar un código de articulo", "HalcoNET", "Aceptar");        
+    }   
+}
+
+
+function LimpiaCodArtNoExistente() {
+    //Se llena la tabla de precios
+    $("#txtPrecio").val('');
+    $("#txtUtilidad").val('');
+    $("#sDescripcionArticulo").text('');
+
+    //--------------------------------------
+    var htmlTable = '';
+    $("#tblLista").html('');
+    $("#tblLista").html(htmlTable);
+
+    $("#tblListaStock").html('');
+    $("#tblListaStock").html(htmlTable);
+    $('#idInformacion').css('display', 'none');
+}
+
+
+function Mensaje(TextMensaje, Titulo, Boton) {
+    if (Publi == 1) {
+        navigator.notification.alert(TextMensaje, null, Titulo, Boton);
+        $.mobile.loading("hide");
+    }
+    else {
+        alert(TextMensaje);
+    }
+}
